be3d5ac4056f27f10c648d0355e146a6f5737e17
RotichKipkoech <kipkoechrottich@gmail.com>
2026-04-09 13:55:49 +0300

    Deployement v1

--- backend/app.py
+++ backend/app.py
@@ -42,7 +42,7 @@ def create_app():
 
     CORS(
     app,
-    origins=["http://localhost:8080", "http://localhost:5173"],
+    origins=["http://localhost:8080", "http://localhost:5173", "https://daibecare.netlify.app"],
     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization"],
     supports_credentials=True
